99d62b632d05603b8af68486eb2fdaf1921c0c77
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2022-09-12 03:14:47 +0530

    Time: 2 ms (65.93%), Space: 44.2 MB (80.98%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,9 +1,11 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
+        // creating a char array to store the individual characters at the correct           // indices
         char[] shuffledString = new char[indices.length];
         for (int i = 0; i < indices.length; i++){
             shuffledString[indices[i]] = s.charAt(i);
         }
+        // convert char array to String
         String result = String.valueOf(shuffledString);
         return result;
     }
