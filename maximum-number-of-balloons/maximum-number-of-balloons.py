33e9a9585009f2a9d7c6f697904f861fb39e1076
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2023-06-20 20:29:19 -0500

    Time: 58 ms, Memory: 16.5 MB - LeetHub

--- maximum-number-of-balloons/maximum-number-of-balloons.py
+++ maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -2,21 +2,34 @@ from collections import defaultdict
 
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        # Better solution: O(n) time, O(n) space
+        balloon_counts = defaultdict(int)
         text_counts = defaultdict(int)
+        for c in "balloon":
+            balloon_counts[c] += 1
         for c in text:
             text_counts[c] += 1
             
-        numBalloons = 0
-        while True:
-            for c in "balloon":
-                if c in text_counts:
-                    text_counts[c] -= 1
-                    if text_counts[c] == 0:
-                        del text_counts[c]
-                else:
-                    return numBalloons
-            numBalloons += 1
+        numBalloons = float('inf')
+        for c in "balloon":
+            numBalloons = min(numBalloons, text_counts[c] // balloon_counts[c])
         return numBalloons
+#         # Brute Force using HashMap: O(m*n) time, O(n) space
+#         text_counts = defaultdict(int)
+#         for c in text:
+#             text_counts[c] += 1
+            
+#         numBalloons = 0
+#         while True:
+#             for c in "balloon":
+#                 if c in text_counts:
+#                     text_counts[c] -= 1
+#                     if text_counts[c] == 0:
+#                         del text_counts[c]
+#                 else:
+#                     return numBalloons
+#             numBalloons += 1
+#         return numBalloons
             
         
         
