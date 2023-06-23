15b313e1ab0a5470d1f263839b996b5751b2c48a
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2023-06-22 22:46:04 -0500

    Time: 48 ms, Memory: 16.6 MB - LeetHub

--- reverse-linked-list-ii/reverse-linked-list-ii.py
+++ reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -5,8 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # traversing upto left index (storing it), reversing nodes between left and right (storing pointers to both ends) and concatenating the everyting to the right of the 'right' pointer.
+        
+        # Edge Cases: SLL length is 0 or 1, or no swapping is needed
         if head is None or head.next is None or left == right:
             return head
+        
+        # traverse upto left index
         curr = head
         position = 1
         while position < left:
@@ -15,6 +20,7 @@ class Solution:
             position += 1
         left_node = curr
         
+        # reverse nodes between left and right 
         prev_node = None
         while position <= right:
             next_node = curr.next
@@ -24,8 +30,11 @@ class Solution:
             
             position += 1
         right_node = prev_node
+        
+        # remainer of SLL nodes
         rest = curr
         
+        # concatenating everything together
         if left != 1:
             prev.next = right_node
         else:
