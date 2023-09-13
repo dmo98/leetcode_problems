a399a54883da86899511dfe387f6a0ad9c1c9666
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2023-09-13 17:02:44 -0500

    Time: 1070 ms, Memory: 16.5 MB - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,39 +1,66 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Approach: Use backtracking with memoization. The helper function takes as argument the remaining amount. At each node in the tree, consider all neighbors (entire array 'coins'), and only expand branches to those children where the child doesn't exceed the remaining amount, prune the other ones. Base case: the remaining amount == 0.
+        # Approach: Bottom-up DP
         
-        def backtrack(rem_amount):
-            """
-            This helper function returns the minimum number of coins needed to sum upto the remaining amount
-            """
-            # base case
-            if rem_amount == 0:
-                return 0
-            
-            # if the sub-tree was already explored somewhere else (in order to not repeat the same computation)
-            if rem_amount in memo:  
-                return memo[rem_amount]
-            
+        dp = [0] * (amount+1)  # each dp[i] will store the minCoins needed to sum upto 'i'
+        
+        # base case is implicit: dp[0] = 0
+        
+        for i in range(1, len(dp)):
             minCoins = float("inf")
             for num in coins:
-                if num > rem_amount:  # prune branch 
+                if num > i: # prune branch
                     continue
                     
-                numCoins = backtrack(rem_amount - num) # recursive call
+                if dp[i-num] == -1: # prune sub-tree
+                    # coins won't sum upto i-num
+                    continue
+                needed_coins = 1 + dp[i-num]
+                minCoins = min(minCoins, needed_coins)
+            
+            if minCoins == float("inf"):
+                dp[i] = -1
+            else:
+                dp[i] = minCoins
                 
-                if numCoins == -1:
-                    continue # prune branch that doesn't sum upto amount
-                minCoins = min(minCoins, 1 + numCoins)
+        return dp[amount]
                 
-            if minCoins == float("inf"): # no child was explored -> dead end
-                memo[rem_amount] = -1
-                return -1
-                
-            memo[rem_amount] = minCoins    
-            return minCoins
-           
-        memo = {}
-        minCoins = backtrack(amount)
         
-        return minCoins
+        
+#         # Top-Down Approach: Use backtracking with memoization. The helper function takes as argument the remaining amount. At each node in the tree, consider all neighbors (entire array 'coins'), and only expand branches to those children where the child doesn't exceed the remaining amount, prune the other ones. Base case: the remaining amount == 0.
+        
+#         def backtrack(rem_amount):
+#             """
+#             This helper function returns the minimum number of coins needed to sum upto the remaining amount
+#             """
+#             # base case
+#             if rem_amount == 0:
+#                 return 0
+            
+#             # if the sub-tree was already explored somewhere else (in order to not repeat the same computation)
+#             if rem_amount in memo:  
+#                 return memo[rem_amount]
+            
+#             minCoins = float("inf")
+#             for num in coins:
+#                 if num > rem_amount:  # prune branch 
+#                     continue
+                    
+#                 numCoins = backtrack(rem_amount - num) # recursive call
+                
+#                 if numCoins == -1:
+#                     continue # prune sub-tree that doesn't sum upto amount
+#                 minCoins = min(minCoins, 1 + numCoins)
+                
+#             if minCoins == float("inf"): # no child was explored -> dead end
+#                 memo[rem_amount] = -1
+#                 return -1
+                
+#             memo[rem_amount] = minCoins    
+#             return minCoins
+           
+#         memo = {}
+#         minCoins = backtrack(amount)
+        
+#         return minCoins
             
