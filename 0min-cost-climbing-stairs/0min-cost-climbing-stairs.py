0b56d0e2e3d17b28781fcbe7f20f4f8f58a95266
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2023-09-13 13:21:10 -0500

    Time: 64 ms, Memory: 16.5 MB - LeetHub

--- 0min-cost-climbing-stairs/0min-cost-climbing-stairs.py
+++ 0min-cost-climbing-stairs/0min-cost-climbing-stairs.py
@@ -1,21 +1,32 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # Approach: Top-down DP
+        # Approach: Bottom-up DP
+        n = len(cost)
+        dp = [0] * (n+1)
+        # base cases are implicitly included: dp[0]=dp[1]=0
         
-        def dp(i):
-            # helper function returns the minimum cost to reach the i'th step.
-            
-            # base cases: dp[0] = dp[1] = 0
-            if i <= 1:
-                return 0
-            
-            if i in memo:
-                return memo[i]
-            
+        for i in range(2,len(cost)+1):
             # Recurrence relation
-            memo[i] = min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
-            return memo[i]
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+            
+        return dp[n]
         
-        memo = {}
-        return dp(len(cost))
+#         # Approach: Top-down DP
+        
+#         def dp(i):
+#             # helper function returns the minimum cost to reach the i'th step.
+            
+#             # base cases: dp[0] = dp[1] = 0
+#             if i <= 1:
+#                 return 0
+            
+#             if i in memo:
+#                 return memo[i]
+            
+#             # Recurrence relation
+#             memo[i] = min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
+#             return memo[i]
+        
+#         memo = {}
+#         return dp(len(cost))
         
