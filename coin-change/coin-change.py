382a0016685db069c1f633c533bcddbdbf44d222
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2023-09-13 17:15:37 -0500

    Time: 991 ms, Memory: 16.6 MB - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -2,29 +2,20 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # Approach: Bottom-up DP
         
-        dp = [0] * (amount+1)  # each dp[i] will store the minCoins needed to sum upto 'i'
+        dp = [float("inf")] * (amount+1)  # each dp[i] will store the minCoins needed to sum upto 'i'
         
-        # base case is implicit: dp[0] = 0
+        # base case
+        dp[0] = 0
         
         for i in range(1, len(dp)):
-            minCoins = float("inf")
-            for num in coins:
-                if num > i: # prune branch
-                    continue
-                    
-                if dp[i-num] == -1: # prune sub-tree
-                    # coins won't sum upto i-num
-                    continue
-                needed_coins = 1 + dp[i-num]
-                minCoins = min(minCoins, needed_coins)
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-c])
             
-            if minCoins == float("inf"):
-                dp[i] = -1
-            else:
-                dp[i] = minCoins
-                
+        if dp[amount] == float("inf"):
+            # coins don't add upto amount
+            return -1
         return dp[amount]
-                
         
         
 #         # Top-Down Approach: Use backtracking with memoization. The helper function takes as argument the remaining amount. At each node in the tree, consider all neighbors (entire array 'coins'), and only expand branches to those children where the child doesn't exceed the remaining amount, prune the other ones. Base case: the remaining amount == 0.
