7fc3600f3cb6abdc7c55ef08863529b04d601aef
Daanish Mohammed <79059856+dmo98@users.noreply.github.com>
2023-09-13 12:37:15 -0500

    Time: 35 ms, Memory: 16.3 MB - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,22 +1,30 @@
+from functools import cache
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Approach: Top-down DP
+        # Approach: Bottom-up DP
+        dp = [1] * (n+1)
         
-        def dp(i):
-            # This helper function returns the number of distinct ways to climb to the i'th step
-            
-            # Base cases: i = 0 or 1
-            if i <= 1:
-                return 1
-            
-            if i in memo:
-                return memo[i]
-            
+        # base cases are implicit: dp[0] = dp[1] = 1
+        for i in range(2,n+1):
             # Recurrence relation
-            memo[i] = dp(i-1) + dp(i-2)
-            return memo[i]
+            dp[i] = dp[i-1] + dp[i-2]
+            
+        return dp[n]
+    
+#         # Approach: Top-down DP
+#         @cache
+#         def dp(i):
+#             # This helper function returns the number of distinct ways to climb to the i'th step
+            
+#             # Base cases: i = 0 or 1
+#             if i <= 1:
+#                 return 1
+            
+#             # Recurrence relation
+#             return dp(i-1) + dp(i-2)
         
-        memo = {}
-        return dp(n)
+#         return dp(n)
+
             
         
