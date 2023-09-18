d825b3f41920f89afba4074906cb14bb40a8d81c
Daanish Mohammed <daanishmd98@gmail.com>
2023-09-18 17:38:01 -0500

    Time: 1266 ms, Memory: 243.6 MB - LeetHub

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,11 +3,13 @@ class Solution:
         # Approach: Top-down DP. 
         
         # The helper function dp(i, holding) returns the maximum profit that can be achieved from day 'i' onwards if we are currently holding stock (True/False).
-        @cache
         def dp(i, holding):
             # base case
             if i >= len(prices):
                 return 0
+            
+            if (i, holding) in memo:
+                return memo[(i, holding)]
             
             # Recurrence relation
             skip = dp(i+1, holding)
@@ -18,8 +20,10 @@ class Solution:
                 # sell stock and pay transaction fee
                 transaction = prices[i] - fee + dp(i+1, False)
                 
-            return max(skip, transaction)
+            memo[(i, holding)] = max(skip, transaction)
+            return memo[(i, holding)]
         
+        memo = {}
         return dp(0, False)
         
         
