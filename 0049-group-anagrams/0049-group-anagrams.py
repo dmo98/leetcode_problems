a662b608657a455ef0c3fad62298eb084ca7d683
Daanish Mohammed <daanishmd98@gmail.com>
2024-02-09 17:00:26 -0600

    Time: 96 ms (38.89%), Space: 21.5 MB (29.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,10 +4,26 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
         for s in strs:
-            key = "".join(sorted(s))
-            hashmap[key].append(s)
+            counts = [0]*26 # array that contains count of each character from a - z
+            for c in s:
+                counts[ord(c) - ord('a')] += 1
+                
+            hashmap[tuple(counts)].append(s)
             
-        answer = []
+        answer = []    
         for key in hashmap.keys():
             answer.append(hashmap[key])
         return answer
+        
+        
+#         # O(n * mlogm) time, O(n*m) space,
+#         # where n = number of strings in 'strs' and m = avg. length of a string
+#         hashmap = defaultdict(list)
+#         for s in strs:
+#             key = "".join(sorted(s))
+#             hashmap[key].append(s)
+            
+#         answer = []
+#         for key in hashmap.keys():
+#             answer.append(hashmap[key])
+#         return answer
