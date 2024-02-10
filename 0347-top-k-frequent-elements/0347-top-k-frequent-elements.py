b27693b66d56f3ce7133eb50eb556dca3651c58e
Daanish Mohammed <daanishmd98@gmail.com>
2024-02-09 23:36:04 -0600

    Time: 92 ms (56.32%), Space: 21.2 MB (70.70%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,5 +6,32 @@ class Solution:
         for num in nums:
             hashmap[num] += 1
             
-        top_k_freq = sorted(set(nums), key=lambda num:hashmap[num], reverse=True)[:k]
-        return top_k_freq
+        # this array stores index:value mapping, where index = frequency of a number, and value = list containing all the numbers that occur as many times as the index value
+        frequencies = [[]] * (len(nums) + 1) # starting from index 0 to index len(nums)
+        for num, count in hashmap.items():
+            frequencies[count] = frequencies[count] + [num]
+            
+        # retrieve the k most frequent numbers
+        top_k_most_freq = []
+        index = len(frequencies)-1
+        while index >= 0:
+            candidates = frequencies[index]
+            while len(candidates) > 0 and k > 0:
+                value = candidates.pop()
+                top_k_most_freq.append(value)
+                k -= 1
+            
+            if k == 0:
+                break
+                
+            index -= 1
+        
+        return top_k_most_freq
+        
+#         # O(nlogn) time, O(n) space
+#         hashmap = defaultdict(int)
+#         for num in nums:
+#             hashmap[num] += 1
+            
+#         top_k_freq = sorted(set(nums), key=lambda num:hashmap[num], reverse=True)[:k]
+#         return top_k_freq
