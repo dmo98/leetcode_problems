32aa46e3ad8f88bb0faf6955241423a42200bede
Daanish Mohammed <daanishmd98@gmail.com>
2024-02-10 17:42:23 -0600

    Time: 755 ms (67.36%), Space: 27.6 MB (55.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,42 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:  
-        # DP approach: TLE
-        def dp(i, bought=False):
-            # Returns the max profit that can be obtained from day i onwards
-            
-            # base case
-            if i >= len(prices):
-                return 0
-            
-            if (i, bought) in memo:
-                return memo[(i, bought)]
-            
-            # recurrence relation:
-            # On day i, we have 2 decisions: buy/sell stock or skip
-            if not bought:
-                buy = -prices[i] + dp(i+1, True)
-                skip = dp(i+1, bought)
-                memo[(i, bought)] = max(buy, skip)
-            else:
-                sell = prices[i]
-                skip = dp(i+1, bought)
-                memo[(i, bought)] = max(sell, skip)
-            return memo[(i, bought)]
+        # Two Pointers approach
+        left = 0
+        right = 1
+        max_profit = 0
+        while right < len(prices):
+            if prices[right] > prices[left]: # it's profitable
+                current_profit = prices[right] - prices[left]
+                max_profit = max(max_profit, current_profit)
+            else: # if not profitable, we have found a new low
+                left = right
+            right += 1
+        return max_profit
+                
         
-        memo = {}
-        return dp(0, 0)
+        
+#         # DP approach: TLE
+#         def dp(i, bought=False):
+#             # Returns the max profit that can be obtained from day i onwards
+            
+#             # base case
+#             if i >= len(prices):
+#                 return 0
+            
+#             if (i, bought) in memo:
+#                 return memo[(i, bought)]
+            
+#             # recurrence relation:
+#             # On day i, we have 2 decisions: buy/sell stock or skip
+#             if not bought:
+#                 buy = -prices[i] + dp(i+1, True)
+#                 skip = dp(i+1, bought)
+#                 memo[(i, bought)] = max(buy, skip)
+#             else:
+#                 sell = prices[i]
+#                 skip = dp(i+1, bought)
+#                 memo[(i, bought)] = max(sell, skip)
+#             return memo[(i, bought)]
+        
+#         memo = {}
+#         return dp(0, 0)
