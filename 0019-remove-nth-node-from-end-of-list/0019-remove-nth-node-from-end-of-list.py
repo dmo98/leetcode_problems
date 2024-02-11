72662ddd021168fb3c9e7dcd620fbbcdc561cdf8
Daanish Mohammed <daanishmd98@gmail.com>
2024-02-11 16:01:03 -0600

    Time: 39 ms (52.01%), Space: 16.5 MB (69.52%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,30 +5,55 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # Brute-force approach
-        # edge case:
+        # Fast and Slow Pointers
         if head.next is None:
             return None
         
-        # find the length of the SLL
-        length = 0
-        curr = head
-        while curr is not None:
-            length += 1
-            curr = curr.next
+        prev = None # node before slow pointer
+        slow = head
+        fast = head
         
-        if length == n:
-            return head.next
-        
-        # iterate upto node at position (length - n) -> node before the one to be deleted
-        position = 1
-        curr = head
-        while curr is not None:
-            if position == length - n:
-                break
-            curr = curr.next
-            position += 1
+        i = 0
+        while i < n:
+            fast = fast.next
+            i += 1
             
-        curr.next = curr.next.next
-        return head            
+        while fast is not None:
+            fast = fast.next
+            prev = slow
+            slow = slow.next
+            
+        # prev pointer is at the node before the one to be deleted
+        if prev is None:
+            return head.next
+        else:
+            prev.next = slow.next
+            return head
+    
+#         # Brute-force approach
+#         # edge case:
+#         if head.next is None:
+#             return None
+        
+#         # find the length of the SLL
+#         length = 0
+#         curr = head
+#         while curr is not None:
+#             length += 1
+#             curr = curr.next
+        
+#         if length == n:
+#             return head.next
+        
+#         # iterate upto node at position (length - n) -> node before the one to be deleted
+#         position = 1
+#         curr = head
+#         while curr is not None:
+#             if position == length - n:
+#                 break
+#             curr = curr.next
+#             position += 1
+            
+#         curr.next = curr.next.next
+#         return head            
         
