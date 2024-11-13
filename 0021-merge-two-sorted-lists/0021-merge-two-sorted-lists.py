6f6ad9b9aae94d1fb7269c8e81d3291752e2f192
Daanish Mohammed <daanishmd98@gmail.com>
2024-11-13 13:15:32 -0600

    Time: 0 ms (100.00%), Space: 16.7 MB (30.66%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,25 +5,44 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Two Pointers
-        p1 = list1
-        p2 = list2
+        # edge cases: empty linked lists
+        if list1 is None:
+            return list2
         
-        answer = ListNode(val=0, next=None) # dummy node, will be removed at the end
-        curr = answer
-        while p1 is not None and p2 is not None: # terminates when one of the SLL is exhausted
-            if p1.val <= p2.val:
-                curr.next = p1
-                p1 = p1.next
-            else:
-                curr.next = p2
-                p2 = p2.next
-            curr = curr.next
-        
-        # empty out one of the SLL (if other is exhausted before)
-        if p1 is not None:
-            curr.next = p1
-        if p2 is not None:
-            curr.next = p2
+        if list2 is None:
+            return list1
 
-        return answer.next
+        # Two Pointers
+        start = True
+
+        while (list1 is not None) and (list2 is not None):
+            if list1.val <= list2.val:
+                if start:
+                    head = list1
+                    curr = head
+                    start = False
+                else:
+                    curr.next = list1
+                    curr = curr.next
+                
+                list1 = list1.next
+            else:
+                if start:
+                    head = list2
+                    curr = head
+                    start = False
+                else:
+                    curr.next = list2
+                    curr = curr.next
+
+                list2 = list2.next
+
+        # attaching the tails if either one isn't exhausted
+        if list1 is not None:
+            curr.next = list1
+        
+        if list2 is not None:
+            curr.next = list2
+        
+        return head
+        
